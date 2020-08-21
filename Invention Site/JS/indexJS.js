4ff15ca39044501a976f624581b1ce3fdb1e7542
PaulVLAD22 <Spongebob007Thebest007>
2020-08-21 22:43:01 +0300

    65

--- Invention Site/JS/indexJS.js
+++ Invention Site/JS/indexJS.js
@@ -1,173 +1,237 @@
 //Title Animation
 const text = document.querySelector(".title");
 const strText = text.textContent;
-const splitText=strText.split("")
-text.textContent="";
+const splitText = strText.split("")
+text.textContent = "";
 
-for (let i=0;i<splitText.length;i++){
-    text.innerHTML += "<span>"+splitText[i]+"</span>";
+for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
 }
 
-let char =0;
-let timer =setInterval(onTick,50);
+let char = 0;
+let timer = setInterval(onTick, 50);
 
 
 
-function onTick(){
+function onTick() {
     const span = text.querySelectorAll('span')[char];
     span.classList.add('fade');
     char++;
-    if(char === splitText.length){
+    if (char === splitText.length) {
         complete();
         return
     }
 }
-function complete(){
+function complete() {
     clearInterval(timer);
-    timer=null;
+    timer = null;
 }
 
 
 
-items=[
+items = [
     {
-        name:"Light bulb",
-        time:"BC",
-        year:"1879"
+        name: "Light bulb",
+        time: "BC",
+        year: "1879"
     }
 ]
 // Read File
-function readTextFile(file) 
-{      
-     var rawFile = new XMLHttpRequest(); 
-     rawFile.open("GET", file, false); 
-     rawFile.onreadystatechange = function () 
-     { 
-         if(rawFile.readyState === 4) 
-         { 
-             if(rawFile.status === 200 || rawFile.status == 0) 
-             { 
-                 var allText = rawFile.responseText; 
-                 readInArray(allText);
-             } 
-         } 
-     };    rawFile.send(null);  
-} 
+function readTextFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                readInArray(allText);
+            }
+        }
+    }; rawFile.send(null);
+}
 
 readTextFile("dataSingleLineBun.txt")
 var array;
-function readInArray(allText){
-    var array=allText.split(',');
-    
-    for (let i=0;i<array.length;i+=3){
-        var Ob={
-            name:array[i],
-            time:array[i+1],
-            year:array[i+2]
+function readInArray(allText) {
+    var array = allText.split(',');
+
+    for (let i = 0; i < array.length; i += 3) {
+        var Ob = {
+            name: array[i],
+            time: array[i + 1],
+            year: array[i + 2]
         }
         items.push(Ob);
     }
 }
 console.log(items);
- 
+
 //game logic
 
-var questionNr=0;
-var score=0;
+var questionNr = 0;
+var score = 0;
+
 // function for choosing random indexes
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
-  }
-var i=0;
-var j=0;
-function renderQuestions(){
-    
-    do{
-        i=getRandomInt(items.length-1);
-        j=getRandomInt(items.length-1);
-    }while(i==j);
-
-    if(questionNr!=0){
-        leftInventionResult(i);
-        rightInventionResult(j);
+}
+var i = 0;
+var j = 0;
+var lastI = 0;
+var lastJ = 0;
+var ansCorrect=0;
+function renderQuestions(e) {
+    mouseX=e.clientX
+    mouseY=e.clientY
+    if (questionNr != 0) {
+        lastI = i;
+        lastJ = j;
     }
-    else{
+    do {
+        i = getRandomInt(items.length - 1);
+        j = getRandomInt(items.length - 1);
+    } while (i == j);
+
+    if (questionNr != 0) {
+        if(questionCorrect(mouseX,mouseY,lastI,lastJ)){
+            leftInventionResultCorrect(lastI);
+            rightInventionResultCorrect(lastJ);
+        }
+        else{
+            leftInventionResultIncorrect(lastI);
+            rightInventionResultIncorrect(lastJ);
+        }
+    }
+    else {
         leftInventionDisplay();
         rightInventionDisplay();
     }
-    questionNr+=1;
+    questionNr += 1;
 }
-function leftInventionResult(i){
-    var leftYear=document.createElement('div');
-    leftYear.id="leftYear";
-    inventionL=items[i].time+" "+items[i].year;
-    leftYear.innerHTML=inventionL;
-    document.getElementById("left-div").appendChild(leftYear);
-    setTimeout(leftInventionRemove,1000);
+function questionCorrect(mouseX,mouseY,lastI,lastJ){
+    if (mouseX<50*screen.width/100){// a ales stanga
+        if (items[lastI].time=="BC" && items[lastJ].time=="BC" && items[lastI].year>items[lastJ].year || items[lastI].time=="BC" && items[lastJ].time=="AD" || items[lastI].time=="AD"&& items[lastJ].time=="AD" && items[lastI].year<items[lastJ].year)
+            return 1;
+        else
+            return 0;
+    }
+    else{//a ales dreapta
+        if (items[lastJ].time=="BC" && items[lastI].time=="BC" && items[lastJ].year>items[lastI].year || items[lastJ].time=="BC" **items[lastI].time=="AD" || items[lastJ].time=="AD" && items[lastI].time=="AD" && items[lastJ].year<items[lastI].year)
+            return 1;
+        else
+            return 0;
+
+    }
+}
+
+function leftInventionResultCorrect(i) {// ai de schimbat 
+    document.getElementById("correct").style.display="flex";
+    ansCorrect=1;
+    var leftYear = document.createElement('div');
+    leftYear.id = "leftYear";
+    inventionL = items[i].time + " " + items[i].year;
+    leftYear.innerHTML = inventionL;
+    document.getElementById("leftCorrect").appendChild(leftYear);
+    setTimeout(leftInventionRemove, 1000);
+    console.log("Result");
+
+}
+function rightInventionResultCorrect(j) {// ai de schimbat
+    document.getElementById("correct").style.display="flex";
+    var rightYear = document.createElement('div');
+    rightYear.id = "rightYear";
+    inventionL = items[j].time + " " + items[j].year;
+    rightYear.innerHTML = inventionL;
+    document.getElementById("rightCorrect").appendChild(rightYear);
+    setTimeout(rightInventionRemove, 1000);
+}
+
+function leftInventionResultIncorrect(i){
+    document.getElementById("incorrect").style.display="flex";
+    ansCorrect=0;
+    var leftYear = document.createElement('div');
+    leftYear.id = "leftYear";
+    inventionL = items[i].time + " " + items[i].year;
+    leftYear.innerHTML = inventionL;
+    document.getElementById("leftIncorrect").appendChild(leftYear);
+    setTimeout(leftInventionRemove, 1000);
     console.log("Result");
 }
-function rightInventionResult(j){
-    var rightYear=document.createElement('div');
-    rightYear.id="rightYear";
-    inventionL=items[j].time+" "+items[j].year;
-    rightYear.innerHTML=inventionL;
-    document.getElementById("right-div").appendChild(rightYear);
-    setTimeout(rightInventionRemove,1000);
+function rightInventionResultIncorrect(j){
+    document.getElementById("incorrect").style.display="flex";
+    var rightYear = document.createElement('div');
+    rightYear.id = "rightYear";
+    inventionL = items[j].time + " " + items[j].year;
+    rightYear.innerHTML = inventionL;
+    document.getElementById("rightIncorrect").appendChild(rightYear);
+    setTimeout(rightInventionRemove, 1000);
 }
 
 
-function leftInventionRemove(){
-    if(questionNr!=0){
+function leftInventionRemove() {
+    if (questionNr != 0) {
         document.getElementById("left-div").removeChild(document.getElementById("stDiv"));
-        document.getElementById("left-div").removeChild(document.getElementById("leftYear"));
+        
     }
     oneSecondLeft();
     console.log("Remove");
 }
-function leftInventionDisplay(){
+function leftInventionDisplay() {
+    // first remove the answer of last question
+    if (questionNr!=0){
+        if(ansCorrect==1){
+            document.getElementById("leftCorrect").removeChild(document.getElementById("leftYear"));
+            document.getElementById("correct").style.display="none";
+        }
+        else{
+            document.getElementById("leftIncorrect").removeChild(document.getElementById("leftYear"));
+            document.getElementById("incorrect").style.display="none";
+
+        }
+    }
     console.log("Display");
-    var stDiv=document.createElement('div');
-    stDiv.id="stDiv";
-    inventionL=items[i].name;
-    stDiv.innerHTML=inventionL;
+    var stDiv = document.createElement('div');
+    stDiv.id = "stDiv";
+    inventionL = items[i].name;
+    stDiv.innerHTML = inventionL;
     document.getElementById("left-div").appendChild(stDiv);
 }
-function rightInventionRemove(){
-    if(questionNr!=0){
+function rightInventionRemove() {
+    if (questionNr != 0) {
         document.getElementById("right-div").removeChild(document.getElementById("drDiv"));
-        document.getElementById("right-div").removeChild(document.getElementById("rightYear"));
     }
     oneSecondRight();
 
 }
-function rightInventionDisplay(){
-    var drDiv=document.createElement('div');
-    drDiv.id="drDiv";
-    inventionR=items[j].name;
-    drDiv.innerHTML=inventionR;
+function rightInventionDisplay() {
+    // first remove the answer of last question
+    if(questionNr!=0){
+        if(ansCorrect==1){
+            document.getElementById("rightCorrect").removeChild(document.getElementById("rightYear"));
+            document.getElementById("correct").style.display="none";
+        }
+        else{
+            document.getElementById("rightIncorrect").removeChild(document.getElementById("rightYear"));
+            document.getElementById("incorrect").style.display="none";
+        }
+    }
+    var drDiv = document.createElement('div');
+    drDiv.id = "drDiv";
+    inventionR = items[j].name;
+    drDiv.innerHTML = inventionR;
     document.getElementById("right-div").appendChild(drDiv);
 }
-function oneSecondLeft(){
-    var timeout=setTimeout(leftInventionDisplay,1000);
+function oneSecondLeft() {
+    var timeout = setTimeout(leftInventionDisplay, 1000);
 }
-function oneSecondRight(){
-    var timeout=setTimeout(rightInventionDisplay,1000);
+function oneSecondRight() {
+    var timeout = setTimeout(rightInventionDisplay, 1000);
 }
 
-document.getElementById('left-div').addEventListener('click',renderQuestions);
-document.getElementById('right-div').addEventListener('click',renderQuestions);
+document.getElementById('left-div').addEventListener('click', renderQuestions);
+document.getElementById('right-div').addEventListener('click', renderQuestions);
 
 // AI DE FACUT SCOR, LEADERBOARD SI LOG IN#
-// NU LASA SA CLICK-uiasca in timpul timeout-ului pana apare inventie noua
-// CAUTA SA ADAUGI SI POZA PT FIECARE DINTRE OBIECTELE DIN ITEMS
-// SI , IN CEL MAI  BUN CAZ, DOWNLAD-EZI PRIMA POZA CARE APARE
-// PE GOOGLE IMAGES SAU ALT SITE
 
-//div verde/rosu
-
-
-
-// poti sa pui peste tot ecranul  un div care la apasare nu face nimic 
-// (in timpul timpului care trece)
-//gen peste body si pui pozitie absoluta width 100% length 100%
+// MAI AI DE CAUTAT POZE CU API PT FIECARE DESCOPERIRE
 
